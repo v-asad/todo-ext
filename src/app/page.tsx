362a4d1a47755ff17f8479fd4fcc81e43ca18dd6
Ahsan <ahsansaleem773@gmail.com>
2025-05-07 01:34:53 +0500

    created sample ui page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,113 @@
+'use client'
+
+import { useState, ChangeEvent } from "react";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly
-          </li>
-        </ol>
+type Task = {
+  description: string;
+  status: 'completed' | 'notCompleted';
+};
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+export default function Home() {
+  const [task, setTask] = useState<string>("");
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const handleAddTask = () => {
+    if (task.trim() === "") {
+      alert("Please add the text.");
+      return;
+    }
+
+    const newTask: Task = { description: task, status: 'notCompleted' };
+    setTasks([newTask, ...tasks]);
+    setTask('');
+  };
+
+  const tasksUpdated = (index: number) => {
+    const updated = [...tasks];
+    updated[index].status = updated[index].status === 'completed' ? 'notCompleted' : 'completed';
+    setTasks(updated);
+  };
+
+  const deleteTask = (index: number) => {
+    const taskToDelete = tasks[index];
+    if (taskToDelete.status === 'completed') {
+      const updated = tasks.filter((_, i) => i !== index);
+      setTasks(updated);
+    }
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTask(e.target.value);
+  };
+
+  return (
+    <div className="p-10 bg-[darkblue]/60 h-screen  ">
+      <div className="flex gap-[10px] justify-start items-center">
+        <Image onClick={()=>handleAddTask} width={30} height={30} alt="home" src={"/assets/home-1-svgrepo-com.svg"}/>
+        <h1 className="text-[30px] text-white ">Tasks</h1>
+      </div>
+      <div className="flex flex-col gap-[5px] justify-center items-start mt-[40px]">
+      {tasks
+          .sort((a, b) => {
+            if (a.status === "completed" && b.status !== "completed") return 1;
+            if (a.status !== "completed" && b.status === "completed") return -1;
+            return 0;
+          })
+          .map((item, index) => (
+     
+      <div key={index}  className="w-full flex justify-between items-center rounded  px-[30px] py-[8px] bg-white">
+      <div className=" flex gap-[10px] justify-center items-start bg-[white]  ">
+        <div>
+        <input
+                type="checkbox"
+                checked={item.status === 'completed'}
+                onChange={() => tasksUpdated(index)}
+              />
+              
+              </div>
+              <div>
+              <p className={item.status === 'completed' ? 'line-through text-gray-500' : ''}>
+                {item.description}
+              </p>
+                <p>date</p>
+              </div>
+      </div>
+      <div>
+      <Image
+                src="/assets/bin.png"
+                width={20}
+                height={20}
+                alt="Delete"
+                onClick={()=>deleteTask(index)}
+                className="cursor-pointer"
+              />
+      </div>
+      </div>
+      
+    ))}
+     
+      
+      </div>
+      <div className="w-full h-full flex justify-center items-end py-[80px]">
+      <div className="w-full static ">
+      
+        <div className="w-full max-w-[1270px] flex gap-[10px] py-[10px] absolute bottom-14 bg-white  rounded px-[10px] overflow-x-hidden">
+        <Image className="" onClick={handleAddTask} alt="adbtn" width={20} height={20} src={"/assets/addbtn.svg"}/>
+          <input
+            value={task}
+            onChange={handleChange}
+            placeholder="Add a task"
+            className="w-full max-w-[600px]  rounded outline-none"
+          />
+          
+          
+          
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+
+       
+      </div>
     </div>
   );
 }
