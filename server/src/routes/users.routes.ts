53e8d188a0475b725e2cce22f33cdbb7225cfc8c
v-asad <asadsohailabid@gmail.com>
2025-06-04 01:13:17 +0500

    Fixed Auth Middleware

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -8,13 +8,10 @@ const usersController = new UsersController();
 usersRouter.post("/login", usersController.login);
 usersRouter.post("/signup", usersController.signup);
 
-// Protect all routes below
-usersRouter.use(authenticateToken);
-
-usersRouter.get("/", usersController.getAll);
-usersRouter.get("/:id", usersController.getOne);
-usersRouter.post("/", usersController.create);
-usersRouter.patch("/:id", usersController.update);
-usersRouter.delete("/:id", usersController.delete);
+usersRouter.get("/", authenticateToken, usersController.getAll);
+usersRouter.get("/:id", authenticateToken, usersController.getOne);
+usersRouter.post("/", authenticateToken, usersController.create);
+usersRouter.patch("/:id", authenticateToken, usersController.update);
+usersRouter.delete("/:id", authenticateToken, usersController.delete);
 
 export default usersRouter;
