b7dd76cbdd052f6cfe9016c0f4c07d4ae41fb9f1
Ahsan <ahsansaleem773@gmail.com>
2025-06-13 01:29:40 +0500

    fix image in login page

--- client/src/components/Illustration/index.tsx
+++ client/src/components/Illustration/index.tsx
@@ -5,7 +5,13 @@ function Illustration() {
   return (
     <div className="w-full min-h-screen bg-[#333333] flex flex-col justify-center items-center gap-[30px]">
       <div className="w-full flex flex-col gap-[24px] items-center justify-center">
-        <Image width={200} height={200} src={'/assets/illustration/logo.png'} alt="logo" />
+        <Image
+          className="w-[200px] h-[150px]"
+          width={100}
+          height={100}
+          src={'/assets/illustration/logo.png'}
+          alt="logo"
+        />
         <h1 className="text-[60px] text-white font-bold">Microsoft To Do</h1>
       </div>
     </div>
